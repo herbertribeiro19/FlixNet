0aba940e4f596217287284da3c952ef7364bad6e
Herbert Ribeiro <hebert11223@gmail.com>
2024-05-11 23:37:52 -0300

    update2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,7 @@ import "./home.css";
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -21,10 +22,20 @@ function Home() {
       });
       //   console.log(response.data.results.slice(0, 10));
       setFilmes(response.data.results.slice(0, 5));
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    );
+  }
+
   return (
     <>
       <h1 className="titleBox">Últimos lançamentos</h1>
