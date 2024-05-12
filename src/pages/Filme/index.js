7aa1b6fe3b6ecfc833aa973c83405941e96ec7c4
Herbert Ribeiro <hebert11223@gmail.com>
2024-05-12 01:14:51 -0300

    new

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,9 +1,64 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./filme.css";
+import api from "../../services/api";
 
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+  // const [desmontar, setDesmontar] = useState(false);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "02b5bfa195025d9b25c4abe3f4ef54e8",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Filme not found");
+        });
+    }
+    loadFilme();
+
+    return () => {
+      console.log("Desmontado");
+      // setDesmontar(true);
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>BEM VINDO A DETALHES DO FILME!</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <strong>Avaliação: {filme.vote_average}/10</strong>
+      <h3>Sinopse:</h3>
+      <span>{filme.overview}</span>
+
+      <div className="area-btn">
+        <button>Salvar</button>
+        <a target="_blank" href="#">
+          <button>Assistir o Trailer</button>
+        </a>
+      </div>
     </div>
   );
 }
